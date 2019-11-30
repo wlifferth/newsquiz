addaebb0e4aa51ce6b38fa8a8ca7110c6669cc12
Will Lifferth <wlifferth@icloud.com>
2019-11-30 15:11:28 -0800

    Added parsers

---
+++ scraping/parsers/nyt_parser.py
@@ -0,0 +1,48 @@
+import sys
+sys.path.append('../../')
+
+from Headline import Headline
+from scraping.parsers import parse_errors
+
+
+PUBLISHER = 'The New York Times'
+
+def parse(item):
+
+    if 'title' in item:
+        text = item['title']
+    else:
+        raise parse_errors.MissingAttributeError('Missing attribute: `title`')
+    if 'published_parsed' in item:
+        datetime = item['published_parsed']
+    else:
+        raise parse_errors.MissingAttributeError('Missing attribute: `published_parsed`')
+    if 'id' in item:
+        link = item['id']
+    else:
+        raise parse_errors.MissingAttributeError('Missing attribute: `id`')
+    if 'author' in item:
+        author = item['author']
+    else:
+        author = None
+    if 'summary' in item:
+        summary = item['summary']
+    else:
+        summary = None
+    if 'media_content' in item and len(item['media_content']) > 0 and 'url' in item['media_content'][0]:
+        image_link = item['media_content'][0]['url']
+        if 'media_credit' in item and len(item['media_credit']) > 0 and 'content' in item['media_credit'][0]:
+            image_credit = item['media_credit'][0]['content']
+    else:
+        image_link = None
+        image_credit = None
+
+    return Headline(
+            text=text,
+            datetime=datetime,
+            publisher=PUBLISHER,
+            link=link,
+            author=author,
+            summary=summary,
+            image_link=image_link,
+            image_credit=image_credit)
---
+++ scraping/parsers/parse_errors.py
@@ -0,0 +1,3 @@
+
+class MissingAttributeError(Exception):
+    pass
